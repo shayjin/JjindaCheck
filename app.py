dd2fbdcf6c9a4577043c825b4a2db62a19a7b867
Jay Shin <shin.810@osu.edu>
2023-03-08 23:25:47 -0500

    된다ㅅㅂㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+from io import StringIO
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from collections import Counter
+
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -9,12 +14,44 @@ data = pd.read_csv('data.csv')
 #data.isnull().sum()
 #data.dropna()
 
-y = data['admit'].values
+def counter_word(text_col):
+    count = Counter()
+    for text in text_col.values:
+        for word in text.split():
+            count[word] += 1
+    return count
+
+def decode(sequence):
+    return " ".join([reverse_word_index.get(idx, "?") for idx in sequence])
+
+
+train_size = int(data.shape[0] * 0.8)
+train_data = data[:train_size]
+
+tokenizer = Tokenizer(num_words=len(counter_word(data['말']))+100)
+
+
+tokenizer.fit_on_texts(data['말'])
+word_index = tokenizer.word_index
+
+
+
+
+train_sequences = tokenizer.texts_to_sequences(data['말'])
+padded = pad_sequences(train_sequences, maxlen=100, padding="post", truncating="post")
+
+
+reverse_word_index = dict([(idx, word) for (word, idx) in word_index.items()])
+
+decoded_text = decode(train_sequences[0])
+
+
+y = data['분류'].values
 
 x = []
 
-for i, rows in data.iterrows():
-    x.append([rows['title'], rows['text']])
+
+
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation='tanh'),
@@ -24,8 +61,32 @@ model = tf.keras.models.Sequential([
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model.fit(np.array(x), np.array(y), epochs=50)
+model.fit(padded, np.array(y), epochs=1)
 
-prediction = model.predict([[1,1]])
+from io import StringIO
 
-print(prediction)
+input = StringIO("""말;
+                    우리집에 닭없음ㅋㅋ j dd 우리집에 닭없음ㅋㅋ 나는 찐따입니다. 우리집에 닭없음ㅋㅋ
+                    """)
+input = pd.read_csv(input, sep=";")
+
+
+sequences = tokenizer.texts_to_sequences(input['말'])
+
+modified = []
+
+
+print(sequences)
+
+print(decode(sequences[0]))
+
+
+val_padded = pad_sequences(sequences, maxlen=100, padding="post", truncating="post")
+
+print("HERE")
+print(input)
+print(sequences)
+print(val_padded)
+prediction = model.predict(val_padded)
+
+print("찐따일 확률은 " + str(float(prediction[0][0]) * 100) + "% 입니다")
